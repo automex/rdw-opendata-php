f30f57955f4b6fd9b6040ad694cfc42119234dde
Patrick Groot <info@patrickgroot.com>
2020-09-08 22:41:00 +0200

    Update test.php

--- tests/test.php
+++ tests/test.php
@@ -16,6 +16,6 @@ var_dump(RDW::get('XP004T'));
 
 // var_dump(RDW::get('XP004T', 'voertuigklasse'));
 
-// var_dump(RDW::get('0001WP', 'voertuigBijzonderheden'));
+// var_dump(RDW::get('0001WP', 'bijzonderheden'));
 
 // var_dump(RDW::get('0005WD', 'voertuigSubcategorie'));
